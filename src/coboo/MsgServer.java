5fefbf6840637862bf735fffd46f13d178f41bdb
zzy <zzy@coboo.com>
2017-03-20 16:49:44 +0800

    beating

--- src/coboo/MsgServer.java
+++ src/coboo/MsgServer.java
@@ -11,6 +11,7 @@ import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -25,14 +26,16 @@ public class MsgServer {
 	AsynchronousServerSocketChannel serverChannel;
 	AsynchronousChannelGroup group = null;
 	static ArrayList<AsynchronousSocketChannel> channelList=new ArrayList<AsynchronousSocketChannel>();
+	static HashMap<Long,AsynchronousSocketChannel>clients=new HashMap<Long, AsynchronousSocketChannel>();
 	private ExecutorService executor;
-	
+	AcceptHandler acceptHandler ;
 	public void start() throws IOException{
 		executor=Executors.newFixedThreadPool(80);
 		group = AsynchronousChannelGroup.withThreadPool(executor);
 		SocketAddress local=new InetSocketAddress(2002);
 		serverChannel=AsynchronousServerSocketChannel.open(group).bind(local);
-		CompletionHandler<AsynchronousSocketChannel, ? super Object> acceptHandler=new AcceptHandler(serverChannel);
+		 acceptHandler = new AcceptHandler(serverChannel);
+		//CompletionHandler<AsynchronousSocketChannel, ? super Object> acceptHandler=acceptHandler2;
 		serverChannel.accept(null,acceptHandler);
 		try {
 			group.awaitTermination(36500, TimeUnit.DAYS);
@@ -41,8 +44,8 @@ public class MsgServer {
 		}
 	}
 	/********************************************************************************************
-	 * 
-	 * @author zzy
+	 *   服务器被成功（失败）连接后该class的方法被调用
+	 *
 	 *
 	 */
 	class AcceptHandler implements CompletionHandler<AsynchronousSocketChannel, Object>{
@@ -58,38 +61,89 @@ public class MsgServer {
 		}
 
 		@Override
-		public void completed(final AsynchronousSocketChannel socketChannelWithResult, Object attachment) {
-			MsgServer.channelList.add(socketChannelWithResult);
-			this.serverChannel1.accept(null,this);
-			socketChannelWithResult.read(buffer, null, new CompletionHandler<Integer, Object>(){
+		public void completed(final AsynchronousSocketChannel client, Object attachment) {
+			try {
+				System.out.println("get connection from client:"+client.getRemoteAddress().toString());
+			} catch (IOException e1) {
+				e1.printStackTrace();
+				return;
+			}
+			//MsgServer.channelList.add(client);
+			this.serverChannel1.accept(null,this);//keep watching
+			client.read(buffer, null, new CompletionHandler<Integer, Object>(){
+				private HashMap<Long,String> sending_message=new HashMap();
 				@Override
 				public void failed(Throwable exc, Object attachment) {	
 					System.out.println("read data failed");
 				}
-
-				@Override
+                /*****************************************************
+                 * 收到数据
+                 */
+				@Override 
 				public void completed(Integer result, Object attachment) {
-					
-					buffer.flip();
-					String str=StandardCharsets.UTF_8.decode(buffer).toString();
-					System.out.println("get string from client"+str);
-					for(AsynchronousSocketChannel sc:MsgServer.channelList){
-						try {
-							sc.getRemoteAddress().toString();
-							sc.write(ByteBuffer.wrap(str.getBytes("utf-8"))).get();
-						} catch (InterruptedException | ExecutionException | IOException e) {
-							e.printStackTrace();
+					//head.flip();
+					if(result>0){
+						System.out.println("received data from result is: "+result);
+						byte[] head=new byte[1];
+						buffer.flip();
+						buffer.get(head, 0, 1);
+						short type=head[0];
+						switch(type){
+						case 100:
+							byte[] icare_id_byte=new byte[8];
+							System.out.println("buffer remains "+buffer.remaining());
+							//buffer.get(icare_id_byte, 0, 8);
+							for (int i = 0; i < 8; i++){
+								icare_id_byte[i] = buffer.get();
+							}
+	                        long icare_id=getLongFromByte(icare_id_byte);
+							System.out.println("beating from "+icare_id);
+							clients.put(icare_id, client);
+							String msg=sending_message.get(icare_id);
+							if(msg!=null){
+								try {
+									client.write(ByteBuffer.wrap(msg.getBytes("utf-8"))).get();
+								} catch (UnsupportedEncodingException | InterruptedException | ExecutionException e) {
+									clients.remove(icare_id);
+									System.out.println("client removed");
+									
+								} 
+							}
+							break;
+						case 1:
+							//message
+							int byte_number=buffer.getInt();
+							Long from_icare_id=buffer.getLong();
+							Long to_icare_id=buffer.getLong();
+					//		clients.put(from_icare_id, client);
+							AsynchronousSocketChannel rclient = clients.get(to_icare_id);
+							String str=StandardCharsets.UTF_8.decode(buffer).toString();
+							if(rclient!=null){
+								try {
+									rclient.write(ByteBuffer.wrap(str.getBytes("utf-8"))).get();
+								} catch (UnsupportedEncodingException | InterruptedException | ExecutionException e) {
+									clients.remove(to_icare_id);
+								}
+							}else{
+								sending_message.put(to_icare_id,str);
+							}
+							break;
 						}
 					}
-					try {
-						socketChannelWithResult.read(buffer,null,this);
-					} catch (Exception e) {
-						
-						e.printStackTrace();
-					}
+
+					//end of switch
+				//	System.out.println("get next data");
+					buffer.clear();
+					client.read(buffer,null,this);
+					return;
 				}
-				
 			});
+		}
+		private long getLongFromByte(byte[] bytes) {
+			ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+		    buffer.put(bytes);
+		    buffer.flip();//need flip 
+		    return buffer.getLong();
 		}
 	}
 	
--- src/coboo/testClient.java
+++ src/coboo/testClient.java
@@ -2,6 +2,7 @@ package coboo;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousChannelGroup;
 import java.nio.channels.AsynchronousSocketChannel;
@@ -11,66 +12,165 @@ import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class testClient {
-
+	InetSocketAddress local = new InetSocketAddress("127.0.0.1",28888);
+	InetSocketAddress remote = new InetSocketAddress("rsy.loopoo.cn",2002);
+//	InetSocketAddress remote = new InetSocketAddress("127.0.0.1",2002);
 	private AsynchronousSocketChannel client;
 	private AsynchronousChannelGroup group;
 	private ExecutorService executor;
 	private ByteBuffer buffer=ByteBuffer.allocate(1024);
-	public void connect(){
+    /****************************************************************
+     * 
+     * @param icare_id
+     * @return
+     */
+	private byte[] getHeartBeating(long icare_id){
+		byte[]msg=new byte[9];
+		byte[]long_bytes=longToByte(icare_id);
+		msg[0]=100;
+		for(int i=0;i<long_bytes.length;i++)msg[i+1]=long_bytes[i];
+		return msg;
+	}
+	public byte[] getMessage(long from,long to,String msgStr){
+		byte[] msg1=msgStr.getBytes();
+		byte[] msg=new byte[1+4+8+8+msg1.length];
+		msg[0]=1;
+		int length=msg.length;
+		appendArray(msg,length,1);
+		appendArray(msg,from,1+4);
+		appendArray(msg,to,1+4+8);
+		appendArray(msg,msg1,1+4+8);
+		return msg;
+	}
+	private void appendArray(byte[]array,byte[] append,int offset){
+		for(int i=offset;i<8+offset;i++){
+			array[i]=append[i-offset];
+		}
+	}
+	private void appendArray(byte[]array,long append,int offset){
+		byte[]longarray=longToByte(append);
+		for(int i=offset;i<8+offset;i++){
+			array[i]=longarray[i-offset];
+		}
+	}
+	private void appendArray(byte[]array,int append,int offset){
+		byte[]retarray=intToByte(append);
+		for(int i=offset;i<4+offset;i++){
+			array[i]=retarray[i-offset];
+		}
+	}
+	
+	/************************************************
+	 * convert a long to a byte array
+	 * @param number
+	 * @return
+	 */
+	 public  byte[] longToByte(long number) { 
+		 ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+		 buffer.putLong(0, number);
+	        return buffer.array();
+	   
+	    } 
+	 /************************************************
+		 * convert a int  to a byte array
+		 * @param number
+		 * @return
+		 */
+		 public  byte[] intToByte(int number) { 
+			 ByteBuffer buffer = ByteBuffer.allocate(Integer.BYTES);
+			 buffer.putInt(0, number);
+		        return buffer.array();
+		    } 
+	/**
+	 * @throws IOException 
+	 * @throws ExecutionException 
+	 * @throws InterruptedException **********************************
+	 * 
+	 */
+	public void connect() throws IOException, InterruptedException, ExecutionException{
 		executor=Executors.newFixedThreadPool(80);
 		try {
 			group=AsynchronousChannelGroup.withThreadPool(executor);
 		} catch (IOException e1) {
 			e1.printStackTrace();
+			return;
 		}
-		try {
-			client=AsynchronousSocketChannel.open(group);
-			client.connect(new InetSocketAddress("rsy.loopoo.cn",2002)).get();
-
-		} catch (IOException | InterruptedException | ExecutionException e) {
-			e.printStackTrace();
-		}
+		client=AsynchronousSocketChannel.open(group);
 		buffer.clear();
+		
+		client.connect(remote).get();
 		client.read(buffer, null, new CompletionHandler<Integer, Object>(){
-
 			@Override
 			public void completed(Integer result, Object attachment) {
 				String content=StandardCharsets.UTF_8.decode(buffer).toString();
 				System.out.println("From server"+content);
 			}
-
 			@Override
 			public void failed(Throwable exc, Object attachment) {
-				// TODO Auto-generated method stub
-
+				System.out.println("read From server failed");
 			}});
+		Thread thread=new Thread(new Runnable(){
+
+			@Override
+			public void run() {
+				while(true){			
+					try {
+						sendBeating(111);
+						System.out.println("send beating");
+						group.awaitTermination(10, TimeUnit.SECONDS);
+					} catch (InterruptedException | ExecutionException e) {
+						System.out.println("reconnecting");
+						break;
+					}
+				}
+				/*try {
+					connect();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}*/
+			}});
+		thread.start();
+		
 	}
-	
-	public static void main(String[] args) {
-		while(true){
-			System.out.println("input a integer(enter 0 exit):");
-			Scanner scanner=new Scanner(System.in);
-	        String str=scanner.next();
-	        if(str.equals("0"))break;
-	        testClient testClient = new testClient();
-	        testClient.connect();
-			testClient.send(str);
-	     
+	/****************************************
+	 * 
+	 * @param str
+	 */
+	private void send(long from,long to,String str) {
+		byte[] msg = this.getMessage(from, to, str);
+		send(msg);
+	}
+	private void send(byte[]msg){
+		try {
+			this.client.write(ByteBuffer.wrap(msg)).get();
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
 		}
 	}
-
-	private void send(String str) {
+	public void sendBeating(long icare_id) throws InterruptedException, ExecutionException{
+			this.client.write(ByteBuffer.wrap(this.getHeartBeating(icare_id))).get();
+	}
+	/**********************************************
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//	while(true){
+		System.out.println("input a integer(enter 0 exit):");
+		Scanner scanner=new Scanner(System.in);
+		String str=scanner.next();
+		//      if(str.equals("0"))break;
+		testClient testClient = new testClient();
 		try {
-			this.client.write(ByteBuffer.wrap(str.getBytes())).get();
-		} catch (InterruptedException | ExecutionException e) {
+			testClient.connect();
+	//		testClient.send(111,222,str);
+		} catch (IOException | InterruptedException | ExecutionException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 	}
-
-	
-
 }
